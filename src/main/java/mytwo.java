052b82c626df0656be1f7b0fad20b6f93302aeaa
Amareshtest <amaresh2014@gmail.com>
2020-04-25 17:57:43 +0530

    New class added to develop branch

---
+++ src/main/java/mytwo.java
@@ -0,0 +1,21 @@
+
+
+public class mytwo {
+
+	
+	public void printt()
+	{
+		System.out.println("New Reposiorydss");
+		System.out.println("New Reposiorydss");
+		System.out.println("New Reposiorydss");
+		System.out.println("New Reposiorydss");
+	}
+
+	public void printt1()
+	{
+		System.out.println("New Reposiorydss");
+		System.out.println("New Reposiorydss");
+		System.out.println("New Reposiorydss");
+		System.out.println("New Reposiorydss");
+	}
+}
