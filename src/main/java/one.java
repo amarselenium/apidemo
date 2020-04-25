0267330ae5cb8d116dd17a9336e8bb46e9bad27b
Amareshtest <amaresh2014@gmail.com>
2020-04-25 17:41:03 +0530

    from the origin

--- src/main/java/one.java
+++ src/main/java/one.java
@@ -4,6 +4,16 @@ public class one {
 	public void print()
 	{
 		System.out.println("New Reposiorydss");
+		System.out.println("New Reposiorydss");
+		System.out.println("New Reposiorydss");
+		System.out.println("New Reposiorydss");
 	}
 
+	public void print1()
+	{
+		System.out.println("New Reposiorydss");
+		System.out.println("New Reposiorydss");
+		System.out.println("New Reposiorydss");
+		System.out.println("New Reposiorydss");
+	}
 }
